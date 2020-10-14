59ea471f528747e0e2d10fba7421f5bae20e3312
Tharun6986 <m.tharunsrikanthreddy@gmail.com>
2020-10-14 11:35:31 +0530

    Email verification done

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -x
 echo "Welcome to user registration system using Regex patterns"
 
-##UserCase2
-echo "Last name has to start with capital letter and with minimum 3 characters"
+##UserCase3
+echo "Email Address"
 
-read -p "Enter Last name: " Name
-Pattern="^[A-Z][a-z]{2,}"
+read -p "Enter a valid Email address: " Email
+Pattern="^[0-9a-zA-Z]+([-+_.][0-9a-zA-Z]+)?@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})?$"
 
-if [[ $Name =~ $Pattern ]]
+if [[ $Email =~ $Pattern ]]
 then
 	echo "Valid"
 else

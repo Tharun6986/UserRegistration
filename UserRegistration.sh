d299f6dd52ac8012aa11192112b93dc02c435a24
Tharun6986 <m.tharunsrikanthreddy@gmail.com>
2020-10-14 12:23:32 +0530

    Password with minimum 8 characters

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -x
 echo "Welcome to user registration system using Regex patterns"
 
-##UserCase4
-echo "Mobile Number"
+##UserCase5
+echo "Rule-1: Password should have minimum 8 characters"
 
-read -p "Enter Mobile number: " MobileNumber
-Pattern="^[0-9]{2} [0-9]{10}$"
+read -p "Enter Password: " Password
+Pattern="[a-zA-Z]{8}"
 
-if [[ $MobileNumber =~ $Pattern ]]
+if [[ $Password =~ $Pattern ]]
 then
 	echo "Valid"
 else

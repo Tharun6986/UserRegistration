e0c2e1a26a2c419da3529c2b831c82c1a34eb8f5
Tharun6986 <m.tharunsrikanthreddy@gmail.com>
2020-10-14 11:23:40 +0530

    UserCase1 done

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,2 +1,16 @@
 #!/bin/bash -x
 echo "Welcome to user registration system using Regex patterns"
+
+##UserCase1
+echo "First name has to start with capital letter and with minimum 3 characters"
+
+read -p "Enter first name: " Name
+Pattern="^[A-Z][a-z]{2,}"
+
+if [[ $Name =~ $Pattern ]]
+then
+	echo "Valid"
+else
+        echo "Invalid"
+fi
+
